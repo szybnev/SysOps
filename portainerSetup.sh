36ea811d3057dd4e74feeb1bb1c409eb9369a754
Zybnev Sergey <s.zybnev@gmail.com>
2023-05-23 23:49:47 +0300

    first version

---
+++ portainerSetup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+printf "Hello, $USER! Portainer will be install now."
+read -p "Enter main port for Portainer: " mainPort
+read -p "Enter additional port for Portainer: " addPort
+
+# Create volume for Portainer
+docker volume create portainer_data
+
+# Start portainer
+docker run -d -p $addPort:8000 -p $mainPort:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ee:latest
+
+# Check Portainer container is up
+docker ps
+
+# Show web link
+printf "https://localhost:$mainPort"
