24f20b8499273fa495302167fe226508e3f175e0
Zybnev Sergey <s.zybnev@gmail.com>
2023-07-18 15:13:39 +0300

    Create lang_install.sh

---
+++ lang_install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Installing Python3"
+sudo apt install -y python3 python3-pip python3-venv
+
+echo
+
+echo "Installing Rust"
+echo "You need to press Enter"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
+rustup check
+
+echo
+
+echo "Installing Golang"
+cd /tmp
+wget https://go.dev/dl/go1.20.6.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20.6.linux-amd64.tar.gz
+echo '' >>  ~/.profile
+echo '# For golang binaries' >> ~/.profile
+echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+echo 'export PATH=$PATH:$HOME/go/bin' >> ~/.profile
+source $HOME/.profile
+rm go*.tar.gz
+cd ~/
+go version
+
+echo "Thank you for using my tool!"
+echo "Github link: https://github.com/szybnev/SysOps"
